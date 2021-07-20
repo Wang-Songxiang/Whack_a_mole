15ea5263089321bb86caa696cb88128b39a3174b
Wang-Songxiang <1374806247@qq.com>
2021-07-20 15:43:49 +0900

    项目失败，择日再战

--- fullautoman.py
+++ fullautoman.py
@@ -8,42 +8,47 @@ def str2sec(t):
     m, s = t.strip("#").split(":")
     return int(m)*60 + int(s) 
 
-titlename = "test.png - ペイント"
+titlename = "PrincessConnectReDive"
 hwnd = win32gui.FindWindow(0, titlename)
 left, top, right, bottom = win32gui.GetWindowRect(hwnd)
-
+win32gui.SetForegroundWindow(hwnd)
 print(left)
 print(top)
 print(right)
 print(bottom)
 
-worksheet = xlrd.open_workbook('shijianzhou.xls')
-sheet_name= worksheet.sheet_names()
-sheet = worksheet.sheet_by_name('Sheet1')
-times = sheet.col_values(0) 
-position = sheet.col_values(1)
-x=len(times)
-print(x)
-for i in range(x):
-    times[i]=str2sec(str(times[i]))
+# worksheet = xlrd.open_workbook('shijianzhou.xls')
+# sheet_name= worksheet.sheet_names()
+# sheet = worksheet.sheet_by_name('Sheet1')
+# times = sheet.col_values(0) 
+# position = sheet.col_values(1)
+# x=len(times)
+# print(x)
+# for i in range(x):
+#     times[i]=str2sec(str(times[i]))
 
-print(times)
-print(position)
-time.sleep(90-times[0])
-for i in range(x):
-    if(position[i]==1):
-        win32api.SetCursorPos((left+500,top+500))
-    elif(position[i]==2):
-        win32api.SetCursorPos((left+550,top+510))
-    elif(position[i]==3):
-        win32api.SetCursorPos((left+560,top+520))
-    elif(position[i]==4):
-        win32api.SetCursorPos((left+570,top+530))
-    elif(position[i]==5):
-        win32api.SetCursorPos((left+580,top+540))
+# print(times)
+# print(position)
+# time.sleep(90-times[0])
+i=1
+while i>0:
+    if(i==1):
+        win32api.SetCursorPos((left+280,bottom-120))
+    elif(i==2):
+        win32api.SetCursorPos((left+440,bottom-120))
+    elif(i==3):
+        win32api.SetCursorPos((left+610,bottom-120))
+    elif(i==4):
+        win32api.SetCursorPos((left+760,bottom-120))
+    elif(i==5):
+        win32api.SetCursorPos((left+920,bottom-120))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
-    if(i<x-1):
-        time.sleep(times[i]-times[i+1])
+    i=i+1
+    if(i>5):
+        i=1
+    time.sleep(3)
+    # if(i<x-1):
+    #     time.sleep(times[i]-times[i+1])
 
 
 # win32api.SetCursorPos([30,150])
